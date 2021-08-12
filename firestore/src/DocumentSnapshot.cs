c36fd0f1499773ca71c5be952baa0c533a8dcb91
varconst <varconst@google.com>
2021-08-12 19:59:40 -0400

    Fix

--- firestore/src/DocumentSnapshot.cs
+++ firestore/src/DocumentSnapshot.cs
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using System.Collections.Generic;
+using BclType = System.Type;
+using Firebase.Firestore.Internal;
+
 namespace Firebase.Firestore {
   /// <summary>
   /// An immutable snapshot of the data for a document.
