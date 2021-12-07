1ba4db9b293c8c618fdec0a2d82c932edd11a21c
Cynthia Jiang <cynthiajiang@google.com>
2021-12-07 14:57:57 -0800

    update build packages

--- build_package.py
+++ build_package.py
@@ -38,6 +38,7 @@ import subprocess
 import zipfile
 import shutil
 
+from github import Github
 from absl import app
 from absl import flags
 from absl import logging
@@ -56,6 +57,10 @@ flags.DEFINE_string("script_folder", "unity_packer",
 
 flags.DEFINE_string("output", "output",
                     "Output folder for unitypackage.")
+
+flags.DEFINE_string("unity_sdk_version", None,
+                    "If None, use the version that reads from guid.json."
+                    "If not None, will check and upgrad cmake/firebase_unity_version.cmake")
 
 flags.DEFINE_boolean("output_upm", False, "Whether output packages as tgz for"
     "Unity Package Manager.")
@@ -128,6 +133,50 @@ def find_pack_script():
     return script_path
   return None
 
+def get_latest_repo_tag(repo_url):
+  repo = Github().get_repo(repo_url)
+  tags = sorted(repo.get_tags(), key=lambda t: t.last_modified)
+  return tags[0].name
+
+def get_ios_pod_version_from_cpp():
+  pod_path = os.path.join(os.getcwd(), "..", "firebase-cpp-sdk", "ios_pod", "Podfile")
+  if not os.path.exists(pod_path):
+    return None
+  with open(pod_path, "r") as f:
+    datafile = f.readlines()
+    for line in datafile:
+      if 'Firebase/Core' in line:
+        result = line.split()
+        return result[-1].strip("\'")
+
+def update_unity_version(unity_sdk_version):
+  version_cmake_path = os.path.join(os.getcwd(), "cmake", "firebase_unity_version.cmake")
+  replacement = ""
+  with open(version_cmake_path, "r") as f:
+    datafile = f.readlines()
+    for line in datafile:
+      if "FIREBASE_UNITY_SDK_VERSION" in line:
+        newline = "set(FIREBASE_UNITY_SDK_VERSION \"" + unity_sdk_version + "\""
+        replacement = replacement + newline + "\n"
+      elif "FIREBASE_IOS_POD_VERSION" in line:
+        podversion = get_ios_pod_version_from_cpp()
+        newline = "set(FIREBASE_IOS_POD_VERSION \"" + podversion + "\""
+        replacement = replacement + newline + "\n"
+      elif "FIREBASE_UNITY_JAR_RESOLVER_VERSION" in line:
+        jar_version = get_latest_repo_tag('googlesamples/unity-jar-resolver')
+        jar_version = jar_version.lstrip("v") # jar resolver need to strip "v" from the tag
+        newline = "set(FIREBASE_UNITY_JAR_RESOLVER_VERSION \"" + jar_version + "\""
+        replacement = replacement + newline + "\n"
+      elif "FIREBASE_CPP_SDK_PRESET_VERSION" in line:
+        cpp_version = get_latest_repo_tag('firebase/firebase-cpp-sdk')
+        newline = "set(FIREBASE_CPP_SDK_PRESET_VERSION \"" + cpp_version + "\""
+        replacement = replacement + newline + "\n"
+      else:
+        replacement = replacement + line
+  
+  with open(version_cmake_path, "w") as fout:
+    fout.write(replacement)
+
 def main(argv):
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
@@ -135,12 +184,21 @@ def main(argv):
   packer_script_path = find_pack_script()
   if packer_script_path == None:
     raise app.UsageError('Cannot find pack script. Please build the project first.')
-
+   
   packer_script_path = os.path.join(os.getcwd(), packer_script_path)
   config_file_path = os.path.join(os.getcwd(), FLAGS.script_folder,
                                   FLAGS.config_file)
   guids_file_path = os.path.join(os.getcwd(), FLAGS.script_folder,
                                  FLAGS.guids_file)
+
+  last_version = None
+  if FLAGS.unity_sdk_version != None:
+    last_version = FLAGS.unity_sdk_version
+    update_unity_version(last_version)
+    return # testing
+  else:
+    last_version = get_last_version(guids_file_path)
+
   zip_file_list = get_zip_files()
 
   if not zip_file_list:
@@ -160,7 +218,7 @@ def main(argv):
       "--output_upm=" + str(FLAGS.output_upm),
   ]
   cmd_args.extend(["--assets_zip=" + zip_file for zip_file in zip_file_list])
-  last_version = get_last_version(guids_file_path)
+  
   if last_version:
     cmd_args.append("--plugins_version=" + last_version)
 
