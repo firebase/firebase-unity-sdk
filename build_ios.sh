b79e4296319fe2b3aeaef942e1e42175c84770e6
Cynthia Jiang <cynthiajiang@google.com>
2022-03-09 22:53:18 -0800

    clean up comments

--- build_ios.sh
+++ build_ios.sh
@@ -112,7 +112,6 @@ function check_exit_code {
  fi
 }
 
-#CMAKE_OPTIONS="-DUNITY_ROOT_DIR=${UNITY_ROOT_DIR}"
 CMAKE_OPTIONS="-DFIREBASE_UNITY_BUILD_TESTS=ON"
 CMAKE_OPTIONS="${CMAKE_OPTIONS} -DFIREBASE_CPP_BUILD_STUB_TESTS=ON" # enable a stub gtest target to get abseil-cpp working.
 
