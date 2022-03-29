ebb370718e13330ef8008a210e84b3375b1e8fa3
Cynthia Jiang <cynthiajiang@google.com>
2022-03-28 20:36:00 -0700

    update linux unity version

--- scripts/gha/print_matrix_configuration.py
+++ scripts/gha/print_matrix_configuration.py
@@ -109,7 +109,7 @@ UNITY_SETTINGS = {
       "ndk": "https://dl.google.com/android/repository/android-ndk-r19-darwin-x86_64.zip"
     },
     _LINUX: {
-      "version": "2019.4.36f1",
+      "version": "2019.4.37f1",
       "packages": {"Default": ["Unity"], "Android": ["Android"], "iOS": ["Ios"], "Windows": ["Windows-mono"], "macOS": ["Mac-mono"], "Linux": None}
     }
   },
