e02080f8e20a294fb6be50b295c63523cd5d716d
Shawn Kuang <chkuang@google.com>
2022-06-23 11:11:56 -0700

    Rollup EDM4U deps to 1.2.172

--- cmake/firebase_unity_version.cmake
+++ cmake/firebase_unity_version.cmake
@@ -21,7 +21,7 @@ set(FIREBASE_IOS_POD_VERSION "9.1.0"
     CACHE STRING "The version of the top-level Firebase Cocoapod to use.")
 
 # https://github.com/googlesamples/unity-jar-resolver
-set(FIREBASE_UNITY_JAR_RESOLVER_VERSION "1.2.171"
+set(FIREBASE_UNITY_JAR_RESOLVER_VERSION "1.2.172"
    CACHE STRING
   "Version tag of Play Services Resolver to download and use (no trailing .0)"
 )
