b9ee611a008e90252e836614b27e5cfff6d9ef8c
a-maurice <amaurice@google.com>
2023-07-13 14:39:42 -0700

    Adjust Installations Race Condition test

--- installations/testapp/Assets/Firebase/Sample/Installations/UIHandlerAutomated.cs
+++ installations/testapp/Assets/Firebase/Sample/Installations/UIHandlerAutomated.cs
@@ -177,9 +177,12 @@ namespace Firebase.Sample.Installations {
             var app = Firebase.FirebaseApp.DefaultInstance;
             installations = Firebase.Installations.FirebaseInstallations.GetInstance(app);
 
-            // Even though the app is destroyed, this call should not result in
-            // any problems, other than returning a potentially faulted task.
-            installations.GetIdAsync();
+            try {
+              installations.GetIdAsync();
+            } catch (System.NullReferenceException) {
+              // Possible NullReferenceException because installations might be still
+              // being disposed from the previous loop.
+            }
 
             app = null;
             installations = null;
