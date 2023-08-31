97d0177567e87a1b3b1e22e4704cc6924e3d55ef
a-maurice <amaurice@google.com>
2023-08-31 11:36:07 -0700

    Pin C++ to a version with updated dependencies

--- cmake/firebase_unity_version.cmake
+++ cmake/firebase_unity_version.cmake
@@ -27,7 +27,7 @@ set(FIREBASE_UNITY_JAR_RESOLVER_VERSION "1.2.177"
 )
 
 # https://github.com/firebase/firebase-cpp-sdk
-set(FIREBASE_CPP_SDK_PRESET_VERSION "v11.4.0"
+set(FIREBASE_CPP_SDK_PRESET_VERSION "ca5cdee69f8ad60955cbfb01801b9161354823aa"
    CACHE STRING
   "Version tag of Firebase CPP SDK to download (if no local or not passed in) and use (no trailing .0)"
 )
