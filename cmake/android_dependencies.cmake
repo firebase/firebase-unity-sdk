feba49d531b8b9dec6f88794c07ebcb6314bda6d
a-maurice <amaurice@google.com>
2023-10-19 10:40:08 -0700

    Undo Android update

--- cmake/android_dependencies.cmake
+++ cmake/android_dependencies.cmake
@@ -15,72 +15,72 @@
 # This file defines the Android dependencies needed by all the modules.
 
 set(FIREBASE_APP_ANDROID_DEPS
-    "com.google.firebase:firebase-common:20.4.2"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-common:20.3.3"
+    "com.google.firebase:firebase-analytics:21.3.0"
     "com.google.android.gms:play-services-base:18.2.0"
 )
 
 set(FIREBASE_ANALYTICS_ANDROID_DEPS
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_APP_CHECK_ANDROID_DEPS
-    "com.google.firebase:firebase-appcheck:17.1.0"
-    "com.google.firebase:firebase-appcheck-debug:17.1.0"
-    "com.google.firebase:firebase-appcheck-playintegrity:17.1.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-appcheck:17.0.1"
+    "com.google.firebase:firebase-appcheck-debug:17.0.1"
+    "com.google.firebase:firebase-appcheck-playintegrity:17.0.1"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_AUTH_ANDROID_DEPS
-    "com.google.firebase:firebase-auth:22.2.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-auth:22.1.2"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_CRASHLYTICS_ANDROID_DEPS
-    "com.google.firebase:firebase-crashlytics-ndk:18.5.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-crashlytics-ndk:18.4.3"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_DATABASE_ANDROID_DEPS
-    "com.google.firebase:firebase-database:20.3.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-database:20.2.2"
+    "com.google.firebase:firebase-analytics:21.3.0"
     "com.google.android.gms:play-services-base:18.2.0"
 )
 
 set(FIREBASE_DYNAMIC_LINKS_ANDROID_DEPS
-    "com.google.firebase:firebase-dynamic-links:21.2.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-dynamic-links:21.1.0"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_FIRESTORE_ANDROID_DEPS
-    "com.google.firebase:firebase-firestore:24.9.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-firestore:24.8.1"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_FUNCTIONS_ANDROID_DEPS
-    "com.google.firebase:firebase-functions:20.4.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-functions:20.3.1"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_INSTALLATIONS_ANDROID_DEPS
-    "com.google.firebase:firebase-installations:17.2.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-installations:17.1.4"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 # iid is needed by messaging to avoid a conflict with functions
 set(FIREBASE_MESSAGING_ANDROID_DEPS
-    "com.google.firebase:firebase-messaging:23.3.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-messaging:23.2.1"
+    "com.google.firebase:firebase-analytics:21.3.0"
     "com.google.firebase:firebase-iid:21.1.0"
     "com.google.flatbuffers:flatbuffers-java:1.12.0"
 )
 
 set(FIREBASE_REMOTE_CONFIG_ANDROID_DEPS
-    "com.google.firebase:firebase-config:21.5.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-config:21.4.1"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
 
 set(FIREBASE_STORAGE_ANDROID_DEPS
-    "com.google.firebase:firebase-storage:20.3.0"
-    "com.google.firebase:firebase-analytics:21.4.0"
+    "com.google.firebase:firebase-storage:20.2.1"
+    "com.google.firebase:firebase-analytics:21.3.0"
 )
