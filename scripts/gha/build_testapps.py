0889f7e668cf42a3cd3b4aea72edf9b98df6b4ca
a-maurice <amaurice@google.com>
2024-03-06 16:22:39 -0800

    Update the ndk version used by the testapp builder

--- scripts/gha/build_testapps.py
+++ scripts/gha/build_testapps.py
@@ -504,12 +504,12 @@ def patch_android_env(unity_version):
   # Set ndk env
   UNITY_SETTINGS = {
     "2020": {
-      _WINDOWS: "https://dl.google.com/android/repository/android-ndk-r19-windows-x86_64.zip",
-      _MACOS: "https://dl.google.com/android/repository/android-ndk-r19-darwin-x86_64.zip",
+      _WINDOWS: "https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip",
+      _MACOS: "https://dl.google.com/android/repository/android-ndk-r21e-darwin-x86_64.zip",
     },
     "2019": {
-      _WINDOWS: "https://dl.google.com/android/repository/android-ndk-r19-windows-x86_64.zip",
-      _MACOS: "https://dl.google.com/android/repository/android-ndk-r19-darwin-x86_64.zip",
+      _WINDOWS: "https://dl.google.com/android/repository/android-ndk-r21e-windows-x86_64.zip",
+      _MACOS: "https://dl.google.com/android/repository/android-ndk-r21e-darwin-x86_64.zip",
     },
   }
   url = UNITY_SETTINGS[str(major_version)][get_desktop_platform()]
