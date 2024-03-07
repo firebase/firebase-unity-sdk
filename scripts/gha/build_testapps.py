8a7ac58c0dc6b8503245f8b7418406eacecbab4f
a-maurice <amaurice@google.com>
2024-03-07 12:40:04 -0800

    Update build_testapps.py

--- scripts/gha/build_testapps.py
+++ scripts/gha/build_testapps.py
@@ -522,9 +522,6 @@ def patch_android_env(unity_version):
       for chunk in r.iter_content(chunk_size=128):
         fd.write(chunk)
 
-    # Print the contents of the ndk_path
-    print("Contents of ndk_path:", os.listdir(ndk_path))
-
     with zipfile.ZipFile(ndk_zip_path, 'r') as zip_ref:
         zip_ref.extractall(ndk_path)
 
@@ -539,6 +536,7 @@ def patch_android_env(unity_version):
     if ndk_direct_folder:
       os.environ["ANDROID_NDK_HOME"] = os.path.abspath(os.path.join(ndk_path, ndk_direct_folder))
       logging.info("set ANDROID_NDK_HOME: %s", os.environ["ANDROID_NDK_HOME"])
+      print("Contents of ndk_direct_folder:", os.listdir(os.path.join(ndk_path, ndk_direct_folder)))
     else:
       logging.warning("No valid android folder unzipped from url %s, ANDROID_NDK_HOME not overwritten", url) 
 
