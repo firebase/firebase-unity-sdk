a95b7989d2867e873c49e8e9ad4f3626e1309e5b
a-maurice <amaurice@google.com>
2025-02-12 14:55:33 -0800

    Support 16 KB page sizes on Android (#1184)
    * Support 16 KB page sizes on Android
    
    * Update readme.md

--- cmake/build_universal.cmake
+++ cmake/build_universal.cmake
@@ -86,6 +86,8 @@ function(build_uni TARGET_LINK_LIB_NAMES PROJECT_LIST_HEADER_VARIABLE)
         "-Wl,--no-undefined"
         # Link against the static libc++, which is the default done by Gradle.
         "-static-libstdc++"
+        # Set the max page size to 16KB, needed by Android 15
+        "-Wl,-z,max-page-size=16384"
     )
     add_custom_command(TARGET firebase_app_uni POST_BUILD
       COMMAND "${ANDROID_TOOLCHAIN_PREFIX}strip" -g -S -d --strip-debug --verbose
