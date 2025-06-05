bde1dc95500fed96e336b84b79f658dcf29f7eb1
Jon Simantov <jsimantov@google.com>
2025-06-05 11:16:12 -0700

    Update Windows runner in build_windows action to windows-latest (from windows-2019) (#1261)
    * Change windows-2019 runner to windows-latest.
    
    * Update libz removal path.
    
    * Update VS version for Windows build.
    
    * Change .NET version.

--- scripts/build_scripts/build_zips.py
+++ scripts/build_scripts/build_zips.py
@@ -430,7 +430,7 @@ def get_windows_args():
       cmake args for windows platform.
   """
   result_args = []
-  result_args.append('-G Visual Studio 16 2019')
+  result_args.append('-G Visual Studio 17 2022')
   result_args.append('-A x64') # TODO flexibily for x32
   result_args.append("-DFIREBASE_PYTHON_HOST_EXECUTABLE:FILEPATH=%s" % sys.executable)
   # Use a newer version of the Windows SDK, as the default one has build issues with grpc
