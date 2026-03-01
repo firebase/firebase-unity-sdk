a5491a58d6451a51473f845c49760b84980c06c8
AustinBenoit <austinbenoit@google.com>
2026-03-01 18:54:57 -0500

    Clean up the file premissions

--- firebaseai/src/LiveGenerativeModel.cs
+++ firebaseai/src/LiveGenerativeModel.cs
@@ -21,8 +21,6 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Firebase.AI.Internal;
-using Firebase.Internal;
 using Google.MiniJSON;
 
 namespace Firebase.AI
