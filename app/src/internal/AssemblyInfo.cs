5700c2935a5d5accf74163a711e115973042a60a
AustinBenoit <austinbenoit@google.com>
2026-04-22 16:58:55 -0400

    Add asmdef for the c# native codebases

---
+++ app/src/internal/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+// Grant native C# Unity packages access to Firebase.App's internal helpers
+[assembly: InternalsVisibleTo("Firebase.Functions")]
+[assembly: InternalsVisibleTo("Firebase.FirebaseAI")]
