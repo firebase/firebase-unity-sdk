8520988a7a05f0a3c590cab42c6862ac3becedad
AustinBenoit <austinbenoit@google.com>
2026-04-22 23:16:27 -0400

    add test app def add allow internal access

--- app/src/internal/AssemblyInfo.cs
+++ app/src/internal/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 // Grant native C# Unity packages access to Firebase.App's internal helpers
 [assembly: InternalsVisibleTo("Firebase.Functions")]
 [assembly: InternalsVisibleTo("Firebase.FirebaseAI")]
+[assembly: InternalsVisibleTo("Firebase.FirebaseAI.TestApp")]
