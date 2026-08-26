c41c74ac4db13aff6be6c1b7334188c9589dbc38
Austin Benoit <austinbenoit@google.com>
2026-08-26 12:29:03 -0400

    refactor(app): use Lazy<string> for AppVersion in FirebaseInterops

--- app/src/internal/FirebaseInterops.cs
+++ app/src/internal/FirebaseInterops.cs
@@ -149,24 +149,23 @@ namespace Firebase.Internal
       return _sdkVersionFetcher.Value;
     }
 
-    private static string _appVersion = null;
+    private static readonly Lazy<string> _appVersionFetcher = new(() =>
+    {
+      try
+      {
+        return Firebase.Platform.PlatformInformation.AppVersion ?? "";
+      }
+      catch (Exception e)
+      {
+        LogError($"Error accessing AppVersion: {e}");
+        return "";
+      }
+    });
 
     // Gets the application version from PlatformInformation.
     internal static string GetApplicationVersion()
     {
-      if (_appVersion == null)
-      {
-        try
-        {
-          _appVersion = Firebase.Platform.PlatformInformation.AppVersion;
-        }
-        catch (Exception e)
-        {
-          LogError($"Error accessing AppVersion: {e}");
-          _appVersion = "";
-        }
-      }
-      return _appVersion ?? "";
+      return _appVersionFetcher.Value;
     }
 
     // Cache the various types and methods needed for AppCheck token retrieval.
